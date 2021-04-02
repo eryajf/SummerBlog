a08001d5fd6feede0ff5d2b9a8b5535255fa8684
summerking <1340904087@qq.com>
2021-04-02 15:48:29 +0800

    4.2

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -22,6 +22,7 @@ module.exports = [
       {text: 'redis', link: '/pages/69521d/'},
       {text: 'elasticsearch', link: '/pages/527279/'},
       {text: 'mysql', link: '/pages/f78c0f/'},
+      {text: 'ceph', link: '/pages/fd1213/'},
       {text: 'spark', link: '/pages/74209d/'},
     ]
   },
